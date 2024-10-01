38bc850c354bea4fd5c405dae3f9d27c56ce4252
sguerrero3 <s.guerrero3@uniandes.edu.co>
2024-09-30 23:47:20 -0500

    Mejorado algunos bugs y parte visual del qr

--- app/src/main/java/com/artlens/view/activities/qrCodeActivity.kt
+++ app/src/main/java/com/artlens/view/activities/qrCodeActivity.kt
@@ -7,6 +7,7 @@ import androidx.activity.compose.setContent
 import androidx.compose.material3.Text
 import androidx.compose.runtime.*
 import androidx.core.content.ContextCompat
+import com.artlens.utils.UserPreferences
 import com.artlens.view.composables.QRCodeScanner
 
 
@@ -19,6 +20,10 @@ class qrCodeActivity : ComponentActivity() {
             var qrCodeValue by remember { mutableStateOf("") }
 
             QRCodeScanner(
+                onBackClick = {navigateToMainActivity()},
+                onHomeClick = {navigateToMainActivity()},
+                onRecommendationClick = {navigateToRecommendations()},
+                onUSerClick = {navigateUser()},
                 onQRCodeScanned = { qrCode ->
                     qrCodeValue = qrCode
                 }
@@ -31,10 +36,36 @@ class qrCodeActivity : ComponentActivity() {
         }
     }
 
+    private fun navigateToMainActivity() {
+        val intent = Intent(this, MainActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToRecommendations() {
+        val intent = Intent(this, RecommendationsActivity::class.java)
+        startActivity(intent)
+    }
+
+    private fun navigateToLogIn() {
+        val intent = Intent(this, LogInActivity::class.java)
+        startActivity(intent)
+    }
+
     private fun artDetail(id: String){
         val intent = Intent(this, ArtworkDetailActivity::class.java)
         intent.putExtra("id", id.toInt())
         startActivity(intent)
+    }
+
+    private fun navigateUser() {
+        val pk = UserPreferences.getPk()
+        if(pk!!>=0) {
+            //Poner Lista de likeados
+        }
+        else{
+            val intent = Intent(this, LogInActivity::class.java)
+            startActivity(intent)
+        }
     }
 
 }
--- app/src/main/java/com/artlens/view/composables/QRCodeScreen.kt
+++ app/src/main/java/com/artlens/view/composables/QRCodeScreen.kt
@@ -12,18 +12,50 @@ import androidx.camera.core.ImageAnalysis
 import androidx.camera.core.ImageProxy
 import androidx.camera.core.Preview
 import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.IconButton
+import androidx.compose.material.Text
+import androidx.compose.material.TextField
+import androidx.compose.material.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.text.input.PasswordVisualTransformation
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
 import androidx.core.content.ContextCompat
 import androidx.lifecycle.LifecycleOwner
+import com.artlens.R
 import com.google.mlkit.vision.barcode.BarcodeScanning
 import com.google.mlkit.vision.barcode.common.Barcode
 import com.google.mlkit.vision.common.InputImage
@@ -33,7 +65,10 @@ import java.util.concurrent.Executors
 @Composable
 fun QRCodeScanner(
     onQRCodeScanned: (String) -> Unit,
-    modifier: Modifier = Modifier
+    onBackClick: () -> Unit,
+    onHomeClick: () -> Unit,
+    onRecommendationClick: () -> Unit,
+    onUSerClick: () -> Unit
 ) {
     val context = LocalContext.current
     val lifecycleOwner = LocalContext.current as LifecycleOwner
@@ -61,40 +96,138 @@ fun QRCodeScanner(
     }
 
     if (hasCameraPermission) {
-        AndroidView(
-            modifier = modifier.fillMaxSize(),
-            factory = { viewContext ->
-                val previewView = androidx.camera.view.PreviewView(viewContext)
-                cameraProviderFuture.addListener({
-                    val cameraProvider = cameraProviderFuture.get()
 
-                    // Preview Use Case
-                    val preview = Preview.Builder().build().also {
-                        it.setSurfaceProvider(previewView.surfaceProvider)
+
+
+        Box(modifier = Modifier
+            .fillMaxSize()
+            .background(Color.White)) {
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+            ) {
+                // Barra superior
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(80.dp)
+                        .background(Color.White),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.SpaceBetween
+                ) {
+                    // Flecha de retroceso
+                    IconButton(onClick = onBackClick) {
+                        Image(
+                            painter = painterResource(id = R.drawable.arrow),
+                            contentDescription = "Back Arrow",
+                            modifier = Modifier.size(30.dp)
+                        )
                     }
 
-                    // ImageAnalysis Use Case for QR code scanning
-                    val imageAnalysis = ImageAnalysis.Builder()
-                        .setTargetResolution(Size(1280, 720))
-                        .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
-                        .build()
-
-                    imageAnalysis.setAnalyzer(scanExecutor) { imageProxy ->
-                        processImageProxy(imageProxy, onQRCodeScanned)
-                    }
-
-                    // Select the back camera as default
-                    val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
-
-                    cameraProvider.unbindAll()
-                    cameraProvider.bindToLifecycle(
-                        lifecycleOwner, cameraSelector, preview, imageAnalysis
+                    // Título centrado
+                    Text(
+                        text = "SIGN UP",
+                        fontSize = 24.sp,
+                        fontWeight = FontWeight.Bold
                     )
-                }, ContextCompat.getMainExecutor(viewContext))
 
-                previewView
+                    // Icono de perfil a la derecha
+                    IconButton(onClick = onUSerClick) {
+                        Image(
+                            painter = painterResource(id = R.drawable.profile),
+                            contentDescription = "Profile Icon",
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+                }
+
+                Spacer(modifier = Modifier.height(16.dp))
+
             }
-        )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(16.dp)
+                    .padding(horizontal = 30.dp),
+                horizontalAlignment = Alignment.Start,
+                verticalArrangement = Arrangement.Center
+
+            ){
+
+            AndroidView(
+
+                factory = { viewContext ->
+                    val previewView = androidx.camera.view.PreviewView(viewContext)
+                    cameraProviderFuture.addListener({
+                        val cameraProvider = cameraProviderFuture.get()
+
+                        // Preview Use Case
+                        val preview = Preview.Builder().build().also {
+                            it.setSurfaceProvider(previewView.surfaceProvider)
+                        }
+
+                        // ImageAnalysis Use Case for QR code scanning
+                        val imageAnalysis = ImageAnalysis.Builder()
+                            .setTargetResolution(Size(1280, 720))
+                            .setBackpressureStrategy(ImageAnalysis.STRATEGY_KEEP_ONLY_LATEST)
+                            .build()
+
+                        imageAnalysis.setAnalyzer(scanExecutor) { imageProxy ->
+                            processImageProxy(imageProxy, onQRCodeScanned)
+                        }
+
+                        // Select the back camera as default
+                        val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+                        cameraProvider.unbindAll()
+                        cameraProvider.bindToLifecycle(
+                            lifecycleOwner, cameraSelector, preview, imageAnalysis
+                        )
+                    }, ContextCompat.getMainExecutor(viewContext))
+
+                    previewView
+                }
+            )}
+
+            // Barra de navegación inferior sobrepuesta
+            Row(
+                modifier = Modifier
+                    .align(Alignment.BottomCenter)
+                    .fillMaxWidth()
+                    .background(Color.White)
+                    .padding(vertical = 8.dp)
+                    .height(50.dp),
+                horizontalArrangement = Arrangement.SpaceEvenly,
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                IconButton(onClick = onHomeClick) {
+                    Image(
+                        painter = painterResource(id = R.drawable.house),
+                        contentDescription = "Home",
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+
+                IconButton(onClick = { /* Acción para ir a Museos */ }) {
+                    Image(
+                        painter = painterResource(id = R.drawable.camera),
+                        contentDescription = "Museos",
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+
+                IconButton(onClick = onRecommendationClick) {
+                    Image(
+                        painter = painterResource(id = R.drawable.fire),
+                        contentDescription = "Recommendations",
+                        modifier = Modifier.size(30.dp)
+                    )
+                }
+            }
+        }
+
     }
 }
 
