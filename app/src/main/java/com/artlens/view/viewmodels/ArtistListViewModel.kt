0dfd746660801751951db54c1c28010f68cc2385
santinajar <111077502+santinajar@users.noreply.github.com>
2024-11-01 10:02:02 -0500

    update de dispachers

--- app/src/main/java/com/artlens/view/viewmodels/ArtistListViewModel.kt
+++ app/src/main/java/com/artlens/view/viewmodels/ArtistListViewModel.kt
@@ -5,6 +5,7 @@ import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.artlens.data.facade.ArtlensFacade
 import com.artlens.data.models.ArtistResponse
+import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
 class ArtistListViewModel(private val facade: ArtlensFacade) : ViewModel() {
@@ -12,9 +13,8 @@ class ArtistListViewModel(private val facade: ArtlensFacade) : ViewModel() {
     // LiveData para observar la lista de artistas
     val artistsLiveData: LiveData<List<ArtistResponse>> = facade.getAllArtists()
 
-    // Función para obtener todos los artistas
     fun fetchAllArtists() {
-        viewModelScope.launch {
+        viewModelScope.launch(Dispatchers.IO) {
             facade.getAllArtists()
         }
     }
