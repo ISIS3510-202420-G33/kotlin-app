7d1d61c19f73fd2339df22568dfee327cc076c46
sguerrero3 <s.guerrero3@uniandes.edu.co>
2024-11-28 01:13:47 -0500

    Protected qr code reader

--- app/src/main/java/com/artlens/view/composables/ArtworkDetailScreen.kt
+++ app/src/main/java/com/artlens/view/composables/ArtworkDetailScreen.kt
@@ -1,10 +1,15 @@
 package com.artlens.view.composables
 
+import android.content.Context
+import android.content.IntentFilter
+import android.net.ConnectivityManager
+import android.net.NetworkCapabilities
 import android.widget.ImageView
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.foundation.text.KeyboardActions
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -15,6 +20,7 @@ import androidx.compose.material.icons.outlined.StarBorder
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
@@ -24,10 +30,12 @@ import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.viewinterop.AndroidView
+import androidx.core.content.ContextCompat.registerReceiver
 import coil.compose.AsyncImage
 import coil.request.ImageRequest
 import com.artlens.R
 import com.artlens.data.models.ArtworkResponse
+import com.artlens.data.services.NetworkReceiver
 
 @Composable
 fun ForumEntry(userName: String, comment: String) {
@@ -56,6 +64,10 @@ fun ArtworkDetailScreen(
     onCrashButtonClick: () -> Unit,
     modifier: Modifier = Modifier // Nuevo parámetro para el modifier
 ) {
+
+
+
+
     Box(modifier = modifier.fillMaxSize()) {
         val context = LocalContext.current
 
@@ -114,24 +126,25 @@ fun ArtworkDetailScreen(
                 }
             }
 
-            // Fila para la estrella de "like"
-            Row(
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp),
-                horizontalArrangement = Arrangement.End
-            ) {
-                IconButton(onClick = onLikeClick) {
-                    Icon(
-                        imageVector = if (isLiked) Icons.Filled.Star else Icons.Outlined.StarBorder,
-                        contentDescription = "Like Artwork",
-                        tint = if (isLiked) likedColor else Color.Gray,
-                        modifier = Modifier.size(30.dp)
-                    )
-                }
-            }
+            // Fila para la estrella de "like
 
             artwork?.let {
+
+                Row(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp),
+                    horizontalArrangement = Arrangement.End
+                ) {
+                    IconButton(onClick = onLikeClick) {
+                        Icon(
+                            imageVector = if (isLiked) Icons.Filled.Star else Icons.Outlined.StarBorder,
+                            contentDescription = "Like Artwork",
+                            tint = if (isLiked) likedColor else Color.Gray,
+                            modifier = Modifier.size(30.dp)
+                        )
+                    }
+                }
                 // Nombre de la obra
                 Text(
                     text = it.fields.name,
@@ -251,7 +264,65 @@ fun ArtworkDetailScreen(
                     )
                 }
             } ?: run {
-                Text(text = "Artwork not found", style = MaterialTheme.typography.body1)
+
+                if (isInternetAvailable(context)) {
+
+                    Text(
+                        text = "Loading...",
+                        fontSize = 20.sp,
+                        fontWeight = FontWeight.Bold,
+                        color = Color.Black
+                    )
+
+                }
+                else {
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.White)
+                    ) {
+                        // Centered Text Fields
+                        Column(
+                            modifier = Modifier
+                                .fillMaxSize()
+                                .padding(16.dp)
+                                .padding(horizontal = 30.dp),
+                            horizontalAlignment = Alignment.CenterHorizontally,
+                            verticalArrangement = Arrangement.Center // This centers the column vertically
+                        ) {
+
+                            Image(
+                                painter = painterResource(id = R.drawable.no_wifi), // Replace with your drawable resource
+                                contentDescription = "Sample Image",
+                                modifier = Modifier
+                                    .size(150.dp) // Adjust the size of the image
+                                    .clip(RoundedCornerShape(12.dp)) // Optional: Rounded corners
+                                    .padding(8.dp)
+                            )
+
+                            Spacer(modifier = Modifier.height(20.dp))
+
+                            Text(
+                                text = "No internet connection",
+                                fontSize = 20.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
+                            )
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                            Text(
+                                text = "Please check your connection and try again",
+                                fontSize = 16.sp,
+                                fontWeight = FontWeight.Bold,
+                                color = Color.Black
+                            )
+
+                            Spacer(modifier = Modifier.height(10.dp))
+
+                        }
+                    }
+                }
             }
         }
 
@@ -290,5 +361,8 @@ fun ArtworkDetailScreen(
                 )
             }
         }
+
     }
 }
+
+
